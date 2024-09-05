dda4ffac82e4c7ee1a04c4e2a6a6da1aa6033463
Noisk8 <juansk8jaramillo@gmail.com>
2024-09-04 20:43:03 -0500

    a ver si que

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 function porro(){
     document.getElementById('presente').innerHTML = 'Gracias por visitar esta web';
-  
 }
+
+// Asegúrate de que la función se ejecute cuando el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', function() {
+    // Aquí puedes llamar a la función porro() si es necesario
+    // porro();
+});
